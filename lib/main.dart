59e465fd9e3aee5475c30b3d22d3b4f8e24e1646
glasspinnA <ozze-1995@hotmail.com>
2019-03-01 21:56:15 +0100

    Created listview with text and background

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,57 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      title: 'MyApp',
+      home: TravelListRoute(),
+    ));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class TravelListRoute extends StatelessWidget {
+  final title = "List demo";
+  final List<String> listText = new List();
+
   @override
   Widget build(BuildContext context) {
-    final title = "List demo";
-    List<String> listText = new List();
-    listText.add("1");
-    listText.add("1");
-    listText.add("1");
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(primarySwatch: Colors.yellow),
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text(title),
-            ),
-            body: ListView.builder(
-              itemCount: listText.length,
-              itemBuilder: (context, index) {
-                return SizedBox(
-                  width: 150.0,
-                  height: 270.0,
-                  child: Card(
-                    child: Stack(
-                      children: <Widget>[
-                        Container(
-                            child: Text(
-                              "Hello World",
-                              style: TextStyle(color: Colors.white),
-                              textAlign: TextAlign.center,
-                            ),
-                            alignment: Alignment.center,
-                            constraints: BoxConstraints.expand(),
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: ExactAssetImage("assets/jonna2.jpg"),
-                                    fit: BoxFit.cover)))
-                      ],
-                    ),
-                  ),
-                );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView.builder(
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return SizedBox(
+            width: 150.0,
+            height: 270.0,
+            child: InkWell(
+              onTap: () {
+                debugPrint("hello");
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SecondRoute(index)));
               },
-            )));
+              child: Card(
+                margin: EdgeInsets.fromLTRB(5, 10, 10, 5),
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                        child: Text(
+                          'Hello World!',
+                          style: TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center,
+                        ),
+                        alignment: Alignment.center,
+                        constraints: BoxConstraints.expand(),
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: ExactAssetImage("assets/jonna2.jpg"),
+                                fit: BoxFit.cover)))
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
